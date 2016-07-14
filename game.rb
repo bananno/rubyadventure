e436da640094c7f768c49e02ee71a22c422509a0
Anna Peterson <peterson.anna.c@gmail.com>
2016-07-14 12:42:57 -0700

    Add a gameplay method

--- game.rb
+++ game.rb
@@ -1,5 +1,44 @@
 class PlayGame
-    puts "hello world"
+    puts "Welcome!"
+
+    def do_something
+        while true
+            separate
+
+            print_map
+
+            puts "\nWhat to do next?"
+
+            action = gets.chomp
+
+            puts "action = " + action
+
+            if action == "quit"
+                separate
+                puts "Goodbye."
+                break
+            else
+                puts "Action not found."
+            end
+        end
+    end
+
+    def separate
+        puts "\n========================\n\n"
+    end
+
+    def print_map
+        puts "| | | | | | | | | |"
+        puts "| | | | | | | | | |"
+        puts "| | | | | | | | | |"
+        puts "| | | | |X| | | | |"
+        puts "| | | | | | | | | |"
+        puts "| | | | | | | | | |"
+        puts "| | | | | | | | | |"
+        puts "| | | | | | | | | |  X = you are here"
+    end
 end
 
-PlayGame
+this_game = PlayGame.new
+
+this_game.do_something
